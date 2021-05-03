f614f37a3c2f63441fda17cafc3d0a65b8f7e9d7
Thomas Wirth <thomas@wtho.de>
2021-05-03 11:47:05 +0200

    feat(build): pages deployment on 'gh-pages'

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,5 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      // ts-jest configuration
-      tsconfig: 'tsconfig.test.json',
-    },
-  },
 }
 export default config
